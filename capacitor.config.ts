f5a7451aa02707499b48dd9633db86a2837e551c
Felix Olszewski <e12022502@student.tuwien.ac.at>
2024-07-30 20:13:54 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capv6-sqlite',
+  webDir: 'www'
+};
+
+export default config;
